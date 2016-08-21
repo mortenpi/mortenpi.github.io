bdbe5993cf9e834fd6782ef79bf6229bee732202
Morten Piibeleht <morten.piibeleht@gmail.com>
2016-08-21 22:14:50 +0200

    Add preliminary GSoC page

---
+++ gsoc2016/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Documentation tools for Julia",
+    "title": "Documentation tools for Julia",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Documentation-tools-for-Julia-1",
+    "page": "Documentation tools for Julia",
+    "title": "Documentation tools for Julia",
+    "category": "section",
+    "text": "This is the submission for the Google Summer of Code 2016 project Documentation tools for Julia, by @mortenpi, mentored by @MichaelHatherly.A big part of Julia is its package ecosystem and their documentation should be as thorough as possible and documenting packages should be made as easy as possible for package developers.The primary goal of the project is to extend Documenter.jl so that it could also generate a detailed reference manual of functions, types, submodules etc. with full signatures and a search capability. The idea is to complement the docstring, since sometimes you need this detailed information and it should be easily accessible (reading the code or using introspection methods is cumbersome).A secondary goal is to lay the groundwork for a comments/notes system, where the community could more easily contribute to the docs (of both Base and packages).note: Note\nThis page was built using Documenter's new HTML output!"
+},
+
+]}
